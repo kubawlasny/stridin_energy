9d18a95fac64e10936316817a5e7d2db5185284b
Jakub Własny <jakub.wlasny@gmail.com>
2016-04-24 22:16:35 +0200

    Improved navigation
    Attaching navigation to fullPage's sections (data-anchors, "active"
    class)

--- stridin_theme/js/custom.js
+++ stridin_theme/js/custom.js
@@ -1,6 +1,10 @@
 /*fullPage*/
 /*http://alvarotrigo.com/fullPage*/
 $(document).ready(function () {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        menu: "#mainMenu",
+        anchors: ['home', 'about', 'offer', 'partners', 'clients', 'team', 'contact'],
+        scrollingSpeed: 900
+    });
 });
 
