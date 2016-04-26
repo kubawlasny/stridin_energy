0258b4aa9d7463755c6d93ad4d8430c5497af8e5
Jakub Własny <jakub.wlasny@gmail.com>
2016-04-26 22:33:45 +0200

    About, offer and partners
    First version of 3 new sections

--- stridin_theme/js/custom.js
+++ stridin_theme/js/custom.js
@@ -4,7 +4,15 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
         menu: "#mainMenu",
         anchors: ['home', 'about', 'offer', 'partners', 'clients', 'team', 'contact'],
-        scrollingSpeed: 900
+        scrollingSpeed: 900,
+        //responsiveWidth: 768,
+        //autoScrolling:false,
+        scrollOverflow: true,
+        navigation: true,
+        
+        //Design
+        verticalCentered: false,
+        paddingTop: '10vh'
     });
 });
 
